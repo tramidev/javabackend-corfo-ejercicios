e0686aa09e231faf4d500d99b8e93acf5afc5ad2
trpzn <tomas@abstractdw.com>
2024-12-11 20:38:35 -0300

    remove old project

--- spring_api_estudiantes/src/main/java/com/talentofuturo/estudiantes/exceptions/EstudianteNotFound.java
+++
@@ -1,9 +0,0 @@
-package com.talentofuturo.estudiantes.exceptions;
-
-public class EstudianteNotFound extends RuntimeException{
-	
-	public EstudianteNotFound(String message) {
-		super(message);
-	}
-	
-}
